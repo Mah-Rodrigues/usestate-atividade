fffd70ed185d57791888c136161bcd5de730e25c
marcelarodrigues6789 <marcelarodrigues6789@gmail.com>
2025-05-20 16:45:26 +0000

    último commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
 import { useState } from "react";
 
-export function App() {
 
+export function App() {
   const [numero, setNumero] = useState(100);
+  const [mostrarEsconder, setMostrarEsconder] = useState(true);
+
 
   function handleAumentar() {
     setNumero(numero + 100);
   }
 
-  return (
+  function handeMostrarEsconder () {
+   // if (mostrarEsconder === true) {
+   //   setMostrarEsconder(false)
+   //   return
+   // }
+      
+   //   setMostrarEsconder(true)
+
+   //   mostrarEsconder === true ? setMostrarEsconder(false) : setMostrarEsconder(true)
+
+      setMostrarEsconder(!mostrarEsconder)
+    }
+
+return (
     <main>
-      <h1>useState - Atividade</h1>
+      <h1>Exercício useState</h1>
 
       <section>
         <h2>Número: {numero}</h2>
-        <button onClick={handleAumentar}>Aumentar</button>
+        <button onClick={handleAumentar}>Aumentar 100</button>
+      </section>
+
+      <section>
+        <h2>O parágrafo que some</h2>
+        {mostrarEsconder ? <p>Em breve irei sumir</p> : ""}
+        <button onClick={handeMostrarEsconder}>{mostrarEsconder ? "Esconder" : "Mostrar"}</button>
       </section>
     </main>
-  ) 
+  );
 }
+
+
+export default App;
